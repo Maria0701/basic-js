717c9ba1f912b8c8993f5b6db618cbd056ad9e02
Masha0701 <reflection2008@gmail.com>
2022-10-17 23:29:12 +0300

    feat: mine-sweaper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function minesweeper( matrix ) {
+  if (!Array.isArray(matrix)) return;
+  var newMatrix = [];
+  var newRow = []
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      var sum = 0
+      if (matrix[row][col + 1] && matrix[row][col + 1] === true) {
+        sum++;
+      }
+      if (matrix[row + 1] && matrix[row + 1][col + 1] && matrix[row + 1][col + 1] === true) {
+        sum++;
+      }
+      if (matrix[row + 1] && matrix[row + 1][col] && matrix[row + 1][col] === true) {
+        sum++;
+      }
+      if (matrix[row + 1] && matrix[row + 1][col - 1] && matrix[row + 1][col - 1] === true) {
+        sum++;
+      }
+      if (matrix[row][col - 1] && matrix[row][col - 1] === true) {
+        sum++;
+      }
+      if (matrix[row-1] && matrix[row-1][col - 1] && matrix[row-1][col - 1] === true) {
+        sum++;
+      }
+      if (matrix[row-1] && matrix[row-1][col] && matrix[row-1][col] === true) {
+        sum++;
+      }
+      if (matrix[row-1] && matrix[row-1][col+1] && matrix[row-1][col+1] === true) {
+        sum++;
+      }
+console.log(sum)
+       newRow.push(sum)
+      if (col === matrix[row].length - 1) {
+        newMatrix.push(newRow);
+        newRow = [];
+      }
+    }
+    
+  }
+  return newMatrix
 }
+
+
 
 module.exports = {
   minesweeper
